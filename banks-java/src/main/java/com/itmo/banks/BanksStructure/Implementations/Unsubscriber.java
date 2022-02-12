7dc2b33bffb002b1e16ac715b1d1d994d369d507
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-12 22:30:49 +0300

    feat: realise class for unsubscribe

---
+++ banks-java/src/main/java/com/itmo/banks/BanksStructure/Implementations/Unsubscriber.java
@@ -0,0 +1,28 @@
+package com.itmo.banks.BanksStructure.Implementations;
+
+import com.itmo.banks.BanksStructure.IMyDisposable;
+import com.itmo.banks.BanksStructure.IMyObserver;
+
+import java.util.ArrayList;
+
+public class Unsubscriber implements IMyDisposable {
+    private final String _name;
+
+    private final ArrayList<IMyObserver> _observers;
+
+    private final IMyObserver _observer;
+
+    public Unsubscriber(String name, ArrayList<IMyObserver> observers, IMyObserver observer) {
+        _name = name;
+        _observers = observers;
+        _observer = observer;
+    }
+
+    public void Dispose() {
+        _observers.remove(_observer);
+    }
+
+    public String GetName() {
+        return _name;
+    }
+}
