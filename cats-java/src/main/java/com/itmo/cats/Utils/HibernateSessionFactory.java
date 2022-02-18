0bd2512c87d1f1145225e89b1032a81009561738
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-18 23:30:44 +0300

    feat: realise HibernateSessionFactory class for creating DB session

--- cats-java/src/main/java/com/itmo/cats/Utils/HibernateSessionFactory.java
+++ cats-java/src/main/java/com/itmo/cats/Utils/HibernateSessionFactory.java
@@ -1,4 +1,32 @@
-﻿package com.itmo.cats.Utils;
+package com.itmo.cats.Utils;
+
+
+import com.itmo.cats.Models.Cat;
+import com.itmo.cats.Models.Owner;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactory() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(Owner.class);
+                configuration.addAnnotatedClass(Cat.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Exception:" + e);
+            }
+        }
+
+        return sessionFactory;
+    }
 }
