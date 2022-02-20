dc65b2f305804baf91ffe2444c372331eda8cde7
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-20 23:59:40 +0300

    feat: add setCatDao and setOwnerDao methods to CatsService

--- cats-java/src/main/java/com/itmo/cats/Services/CatsService.java
+++ cats-java/src/main/java/com/itmo/cats/Services/CatsService.java
@@ -1,16 +1,24 @@
 package com.itmo.cats.Services;
 
-import com.itmo.cats.Dao.CatDao;
-import com.itmo.cats.Dao.OwnerDao;
+import com.itmo.cats.Dao.Implementations.CatDao;
+import com.itmo.cats.Dao.Implementations.OwnerDao;
 import com.itmo.cats.Models.Cat;
 import com.itmo.cats.Models.Owner;
 
 import java.util.ArrayList;
 
 public class CatsService {
-    private final OwnerDao _ownerDao = new OwnerDao();
+    private OwnerDao _ownerDao = new OwnerDao();
 
-    private final CatDao _catDao = new CatDao();
+    private CatDao _catDao = new CatDao();
+
+    public void setCatDao(CatDao catDao){
+        _catDao = catDao;
+    }
+
+    public void setOwnerDao(OwnerDao ownerDao){
+        _ownerDao = ownerDao;
+    }
 
     public Owner getOwner(int id){
         return _ownerDao.getById(id);
