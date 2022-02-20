eaab8dfe1d14429d2aea502f893742c2dc2987af
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-20 23:59:03 +0300

    feat: add implementation of corresponding interfaces to Dao classes

--- cats-java/src/main/java/com/itmo/cats/Dao/Implementations/CatDao.java
+++ cats-java/src/main/java/com/itmo/cats/Dao/Implementations/CatDao.java
@@ -1,43 +1,43 @@
-package com.itmo.cats.Dao;
+package com.itmo.cats.Dao.Implementations;
 
+import com.itmo.cats.Dao.ICatDao;
 import com.itmo.cats.Models.Cat;
-import com.itmo.cats.Models.Owner;
 import com.itmo.cats.Utils.HibernateSessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 
-public class OwnerDao {
-    public Owner getById(Integer id){
-        return HibernateSessionFactory.getSessionFactory().openSession().get(Owner.class, id);
+public class CatDao implements ICatDao {
+    public Cat getById(Integer id){
+        return HibernateSessionFactory.getSessionFactory().openSession().get(Cat.class, id);
     }
 
-    public void save(Owner owner){
+    public void save(Cat cat){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        session.save(owner);
+        session.save(cat);
         transaction.commit();
         session.close();
     }
 
-    public void update(Owner owner){
+    public void update(Cat cat){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        session.update(owner);
+        session.update(cat);
         transaction.commit();
         session.close();
     }
 
-    public void delete(Owner owner){
+    public void delete(Cat cat){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        session.delete(owner);
+        session.delete(cat);
         transaction.commit();
         session.close();
     }
 
-    public ArrayList<Owner> getAll(){
-        return (ArrayList<Owner>) HibernateSessionFactory.getSessionFactory().openSession().createQuery("From Owner").list();
+    public ArrayList<Cat> getAll(){
+        return (ArrayList<Cat>) HibernateSessionFactory.getSessionFactory().openSession().createQuery("From Cat").list();
     }
 }
--- cats-java/src/main/java/com/itmo/cats/Dao/Implementations/OwnerDao.java
+++ cats-java/src/main/java/com/itmo/cats/Dao/Implementations/OwnerDao.java
@@ -1,42 +1,43 @@
-package com.itmo.cats.Dao;
+package com.itmo.cats.Dao.Implementations;
 
-import com.itmo.cats.Models.Cat;
+import com.itmo.cats.Dao.IOwnerDao;
+import com.itmo.cats.Models.Owner;
 import com.itmo.cats.Utils.HibernateSessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 
-public class CatDao {
-    public Cat getById(Integer id){
-        return HibernateSessionFactory.getSessionFactory().openSession().get(Cat.class, id);
+public class OwnerDao implements IOwnerDao {
+    public Owner getById(Integer id){
+        return HibernateSessionFactory.getSessionFactory().openSession().get(Owner.class, id);
     }
 
-    public void save(Cat cat){
+    public void save(Owner owner){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        session.save(cat);
+        session.save(owner);
         transaction.commit();
         session.close();
     }
 
-    public void update(Cat cat){
+    public void update(Owner owner){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        session.update(cat);
+        session.update(owner);
         transaction.commit();
         session.close();
     }
 
-    public void delete(Cat cat){
+    public void delete(Owner owner){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        session.delete(cat);
+        session.delete(owner);
         transaction.commit();
         session.close();
     }
 
-    public ArrayList<Cat> getAll(){
-        return (ArrayList<Cat>) HibernateSessionFactory.getSessionFactory().openSession().createQuery("From Cat").list();
+    public ArrayList<Owner> getAll(){
+        return (ArrayList<Owner>) HibernateSessionFactory.getSessionFactory().openSession().createQuery("From Owner").list();
     }
 }
