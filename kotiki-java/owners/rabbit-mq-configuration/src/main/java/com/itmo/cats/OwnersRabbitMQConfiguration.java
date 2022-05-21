52a1a0d79194c958ff56c630041cf5d1886ce1a7
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-05-21 22:16:58 +0300

    feat: add listener OwnersRabbitMQConfiguration

---
+++ kotiki-java/owners/rabbit-mq-configuration/src/main/java/com/itmo/cats/OwnersRabbitMQConfiguration.java
@@ -0,0 +1,64 @@
+package com.itmo.cats;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OwnersRabbitMQConfiguration {
+
+    @Bean
+    public DirectExchange ownersDirectExchange() {
+        return new DirectExchange("owners-exchange");
+    }
+
+    @Bean
+    public Queue ownersGetByIdQueue() {
+        return new Queue("ownersGetByIdQueue");
+    }
+
+    @Bean
+    public Queue ownersAddQueue() {
+        return new Queue("ownersAddQueue");
+    }
+
+    @Bean
+    public Queue ownersUpdateQueue() {
+        return new Queue("ownersUpdateQueue");
+    }
+
+    @Bean
+    public Queue ownersGetAllQueue() {
+        return new Queue("ownersGetAllQueue");
+    }
+
+    @Bean
+    public Queue ownersDeleteByIdQueue() {
+        return new Queue("ownersDeleteByIdQueue");
+    }
+
+    @Bean
+    public Binding bindOwnersGetByIdQueue() {
+        return BindingBuilder.bind(ownersGetByIdQueue()).to(ownersDirectExchange()).with("ownersGetByIdQueue");
+    }
+
+    @Bean
+    public Binding bindOwnersAddQueue() {
+        return BindingBuilder.bind(ownersAddQueue()).to(ownersDirectExchange()).with("ownersAddQueue");
+    }
+
+    @Bean
+    public Binding bindOwnersUpdateQueue() {
+        return BindingBuilder.bind(ownersUpdateQueue()).to(ownersDirectExchange()).with("ownersUpdateQueue");
+    }
+
+    @Bean
+    public Binding bindOwnersGetAllQueue() {
+        return BindingBuilder.bind(ownersGetAllQueue()).to(ownersDirectExchange()).with("ownersGetAllQueue");
+    }
+
+    @Bean
+    public Binding bindOwnersDeleteByIdQueue() {
+        return BindingBuilder.bind(ownersDeleteByIdQueue()).to(ownersDirectExchange()).with("ownersDeleteByIdQueue");
+    }
+}
