9341ffb17d8d7e815fd9b973f91f8d4ddb4b379d
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-05-24 22:49:42 +0300

    feat: add ComponentScan annotation to WebApplication

--- kotiki-java/user-interface/web/src/main/java/com/itmo/cats/WebApplication.java
+++ kotiki-java/user-interface/web/src/main/java/com/itmo/cats/WebApplication.java
@@ -3,8 +3,10 @@ package com.itmo.cats;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.itmo.cats.repository"})
 @EnableRabbit
 public class WebApplication {
 
